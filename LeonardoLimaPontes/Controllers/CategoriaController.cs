430b078a716e98118bd80747285da829006ba8e9
Leonardo Lima Pontes <bruxodeveloper@hotmail.com>
2021-07-31 20:01:25 -0300

    adicionado nova rota para categoria

---
+++ LeonardoLimaPontes/Controllers/CategoriaController.cs
@@ -0,0 +1,26 @@
+﻿using LeonardoLimaPontes.Data;
+using LeonardoLimaPontes.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeonardoLimaPontes.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoriaController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Categoria> objList = _db.Categoria;
+            return View();
+        }
+    }
+}
